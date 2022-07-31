8c2347bf7195413e462e5725054432a93563a1b6
Manu <ominuka.mauro@gmail.com>
2022-07-31 11:12:18 -0300

    Create a user

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,19 @@
-import User from '../models/user.model'
+import User, { UserDocument, UserInput } from '../models/user.model'
+import { omit } from 'lodash'
+import { FilterQuery } from 'mongoose'
 
-const login = async (email: number) => {
+export const createUser = async (input: UserInput) => {}
+
+export const validatePassword = async ({
+  email,
+  password,
+}: {
+  email: string
+  password: string
+}) => {
   const user = await User.findOne({ email })
-  user.
+  if (!user) return false
+  const isValid = await user.verifyPassword(password)
+  if (!isValid) return false
+  return omit(user.toJSON(), 'password')
 }
